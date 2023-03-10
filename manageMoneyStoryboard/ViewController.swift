d6192dd45caaf99f40992f9fe75abddcd2354c39
park <kacian93@gmail.com>
2023-03-10 20:06:00 +0900

    Initial Commit

---
+++ manageMoneyStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  manageMoneyStoryboard
+//
+//  Created by PC156 on 2023/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
